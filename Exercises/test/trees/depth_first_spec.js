b880c0ba44812a2924f8bc2ae59096bbabe03f94
Ryan Sobol <contact@ryansobol.com>
2016-10-07 13:36:04 -0700

    Fix depth first test

--- Exercises/test/trees/depth_first_spec.js
+++ Exercises/test/trees/depth_first_spec.js
@@ -21,19 +21,19 @@ describe('depth first search', function() {
     })
 
     it('returns values ordered by depth first search, pre-order', function() {
-      expect(depthFirst.preOrder(this.a)).to.equal(
+      expect(depthFirst.preOrder(this.a)).to.deep.equal(
         ['A', 'B', 'D', 'E', 'C']
       );
     });
 
     it('returns values ordered by depth first search, in-order', function() {
-      expect(depthFirst.inOrder(this.a)).to.equal(
+      expect(depthFirst.inOrder(this.a)).to.deep.equal(
         ['D', 'B', 'E', 'A', 'C']
       );
     });
 
     it('returns values ordered by depth first search, post-order', function() {
-      expect(depthFirst.postOrder(this.a)).to.equal(
+      expect(depthFirst.postOrder(this.a)).to.deep.equal(
         ['D', 'E', 'B', 'C', 'A']
       );
     });
