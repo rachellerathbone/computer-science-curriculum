442bc4d4e9910f2fc3df2ecbfe541f6fb8240692
Michael Herman <hermanmu@gmail.com>
2016-12-01 14:47:09 -0700

    added hash example

--- Exercises/src/hash-tables/kth_largest_element.js
+++ Exercises/src/hash-tables/kth_largest_element.js
@@ -2,9 +2,6 @@
 
 Given an array of integers find the kth element in the sorted order. Use a brute force method. Then refactor using a hash within the function for optimization.
 
-So, if the array is [3, 1, 2, 1, 4] and k is 3 then the result is 2, because it’s the 3rd element in sorted order (but the 3rd distinct element is 3).
-
-
 largestElement(2, [3, 5, 6, 8]) // => 5
 largestElement(3, [3, 1, 2, 1, 4]) // => 2
 
